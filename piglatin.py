88b3ba8faced7e9d37753d2cd5caeb7cc77e3afc
nyccowgirl <nyc_cowgirl@hotmail.com>
2017-12-10 18:50:23 -0800

    add igpay atinlay

---
+++ piglatin.py
@@ -0,0 +1,46 @@
+"""Turn a phrase into Pig Latin.
+
+This takes a space-separated phrase and returns it in Pig Latin.
+
+Rules:
+
+1. If the word begins with a consonant (not a, e, i, o, u),
+   move the first letter to the end and add 'ay'
+
+2. If the word begins with a vowel, add 'yay' to the end
+
+For example:
+
+    >>> pig_latin('hello awesome programmer')
+    'ellohay awesomeyay rogrammerpay'
+
+"""
+
+
+def pig_latin(phrase):
+    """Turn a phrase into pig latin.
+
+    There will be no uppercase letters or punctuation in the phrase.
+
+        >>> pig_latin('hello awesome programmer')
+        'ellohay awesomeyay rogrammerpay'
+    """
+
+    lst = phrase.split(' ')
+    igpay_atinlay = []
+
+    vowels = (['a', 'e', 'i', 'o', 'u'])
+
+    for word in lst:
+        if word[0] in vowels:
+            igpay_atinlay.append(word + 'yay')
+        else:
+            igpay_atinlay.append(word[1:] + word[0] + 'ay')
+
+    return " ".join(igpay_atinlay)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. REATGAY OBJAY!\n"
