e64ae76ecc5eac59d27cceccbc89afdb7210515d
nyccowgirl <nyc_cowgirl@hotmail.com>
2017-12-10 19:06:08 -0800

    add prime numbers

--- piglatin.py
+++ piglatin.py
@@ -40,6 +40,45 @@ def pig_latin(phrase):
     return " ".join(igpay_atinlay)
 
 
+    # A helper function:
+
+# def pig_latin_word(word):
+#     """Turn a word into the pig latin version.
+
+#     For example::
+
+#         >>> pig_latin_word('porcupine')
+#         'orcupinepay'
+
+#         >>> pig_latin_word('apple')
+#         'appleyay'
+#     """
+
+#     if word[0] in 'aeiou':
+#         return word + 'yay'
+
+#     else:
+#         return word[1:] + word[0] + 'ay'
+# The main function:
+
+# def pig_latin(phrase):
+#     """Turn a phrase into pig latin.
+
+#     There will be no uppercase letters or punctuation in the phrase.
+
+#         >>> pig_latin('hello awesome programmer')
+#         'ellohay awesomeyay rogrammerpay'
+#     """
+
+#     # START SOLUTION
+
+#     words = phrase.split(' ')
+
+#     pl_words = [pig_latin_word(w) for w in words]
+
+#     return " ".join(pl_words)
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
---
+++ primes.py
@@ -0,0 +1,115 @@
+"""Return count number of prime numbers, starting at 2.
+
+For example::
+
+    >>> primes(0)
+    []
+
+    >>> primes(1)
+    [2]
+
+    >>> primes(5)
+    [2, 3, 5, 7, 11]
+
+"""
+
+
+def primes(count):
+    """Return count number of prime numbers, starting at 2."""
+
+    prime = []
+
+    nums = range(2, 100)
+    for i in range(2, 8):
+        nums = filter(lambda x: x == i or x % i, nums)
+
+    while count > len(prime):
+        prime.append(nums.pop(0))
+
+    return prime
+
+
+#     def is_prime(num):
+#     """Is num a prime number?
+
+#     num will always be a positive integer.
+
+#     >>> is_prime(0)
+#     False
+
+#     >>> is_prime(1)
+#     False
+
+#     >>> is_prime(2)
+#     True
+
+#     >>> is_prime(3)
+#     True
+
+#     >>> is_prime(4)
+#     False
+
+#     >>> is_prime(7)
+#     True
+
+#     >>> is_prime(25)
+#     False
+#     """
+
+#     assert num >= 0, "Num should be a positive integer!"
+
+#     # definition: 0 and 1 are not prime
+#     if num < 2:
+#         return False
+
+#     # definition: 2 is prime
+#     if num == 2:
+#         return True
+
+#     # if it's divisible by 2, it's not prime
+#     # (We do this as a special case, so that after this we can check
+#     # only odd numbers -- all even numbers are divisible by 2)
+#     if num % 2 == 0:
+#         return False
+
+#     # see if number is prime -- we'll do this by checking
+#     # to see if there's any odd number 3 .. sqrt(num)
+#     # that evenly divides num (why square root? think about it!)
+
+#     n = 3
+
+#     while n * n <= num:
+#         if num % n == 0:
+#             return False
+#         # Go to next odd number
+#         n += 2
+
+#     return True
+# it treats 2 as a special case (2 is a prime number) and multiples of 2 are not.
+# it then only has to check whether the number is divisible by prime numbers
+# it only checks up to sqrt(num) – why? Think about it!
+# We then made a primes function:
+
+# def primes(count):
+#     """Return count number of prime numbers, starting at 2."""
+
+#     # START SOLUTION
+
+#     primes = []
+#     num = 2
+
+#     while count > 0:
+
+#         if is_prime(num):
+#             primes.append(num)
+#             count -= 1
+
+#         num += 1
+
+#     return primes
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. GREAT WORK!\n"
