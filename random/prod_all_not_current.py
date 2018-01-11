36377e552079f3a0fd8f1502da7799d36ea1bb8d
nyccowgirl <nyc_cowgirl@hotmail.com>
2018-01-10 16:20:02 -0800

    add product of all items except current

---
+++ random/prod_all_not_current.py
@@ -0,0 +1,49 @@
+from operator import mul
+
+
+def products_all_nums_but_current(lst):
+    """Given a list of integers you want to find the product of all the
+    integers except the integer at the current index.
+    Write a function that takes a list of integers and returns a
+    list of products.
+
+    DO NOT USE DIVISION
+
+    >>> products_all_nums_but_current([1, 2, 3, 4])
+    [24, 12, 8, 6]
+
+    >>> products_all_nums_but_current([2, 4, 0, 1])
+    [0, 0, 8, 0]
+
+    >>> products_all_nums_but_current([3, 3, 3])
+    [9, 9, 9]
+
+    >>> products_all_nums_but_current([])
+    []
+
+    >>> products_all_nums_but_current([1])
+    [1]
+    """
+
+    prod_lst = []
+
+    if len(lst) <= 1:
+        return lst
+
+    for i in range(len(lst)):
+        if i == 0:
+            prod_lst.append(reduce(mul, lst[i + 1:]))
+        elif i == (len(lst) - 1):
+            prod_lst.append(reduce(mul, lst[:-1]))
+        else:
+            prod_lst.append(reduce(mul, lst[:i]) * reduce(mul, lst[i + 1:]))
+
+    return prod_lst
+
+
+################################################################################
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. GREAT!\n"
