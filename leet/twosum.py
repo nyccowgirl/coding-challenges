340ddd686edcf548a4f4cb33a01f80cba36ed75e
Trang Hoang <trang.d.hoang@gmail.com>
2021-03-02 21:00:17 +0000

    completed two sum challenge for java & C++

--- leet/twosum.py
+++ leet/twosum.py
@@ -33,16 +33,17 @@ Only one valid answer exists.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums[i + 1:], start = i + 1):  
-                if (num1 + num2 == target):
-                    return [i, j]
+#         for i, num1 in enumerate(nums):
+#             for j, num2 in enumerate(nums[i + 1:], start = i + 1):  
+#                 if (num1 + num2 == target):
+#                     return [i, j]
         
 #         using dict
         
-#         seen = {}
+        seen = {}
         
-#         for i, num in enumerate(nums):
-#             if (target - num) in seen:
-#                 return [seen[target - num], i]
-#             seen[num] = i
+        for i, num in enumerate(nums):
+            if (target - num) in seen:
+                return [seen[target - num], i]
+            seen[num] = i
+
