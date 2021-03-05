7a39d31f363d26b2e495af203900fd4f5a3ba0e9
Trang Hoang <trang.d.hoang@gmail.com>
2021-03-05 20:57:47 +0000

    added addtwonums java version in leet

--- leet/Python/addtwonums.py
+++ leet/Python/addtwonums.py
@@ -3,10 +3,8 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
- 
 
 Example 1:
-
 
 Input: l1 = [2,4,3], l2 = [5,6,4]
 Output: [7,0,8]
@@ -27,6 +25,7 @@ The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
 """
+
 
 # Definition for singly-linked list.
 class ListNode:
