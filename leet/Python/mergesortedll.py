89cc007cf179bef996c196b9350568368adfb403
Trang Hoang <trang.d.hoang@gmail.com>
2021-04-07 06:54:13 +0000

    add mergesortedll for python

---
+++ leet/Python/mergesortedll.py
@@ -0,0 +1,73 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. 
+The list should be made by splicing together the nodes of the 
+first two lists.
+
+Example 1:
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+Input: l1 = [], l2 = [0]
+Output: [0]
+ 
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # Option 1 - iterative:
+        head = tail = ListNode()
+        
+        # while both linked lists have nodes to traverse, compare the values
+        # and add smaller value to tail, moving to the next node in the
+        # respective list
+        while l1 and l2:
+            if l1.val <= l2.val:
+                # tail.next = ListNode(l1.val)
+                # tacking on entire list seems to run faster than creating
+                # new node
+                tail.next = l1      
+                l1 = l1.next
+            else:
+                # tail.next = ListNode(l2.val)
+                tail.next = l2
+                l2 = l2.next
+            
+            # update tail
+            tail = tail.next
+            
+        # add remaining list to end of tail
+        tail.next = l1 or l2
+        
+        return head.next
+        
+#         # Option 2 - recursive (longer runtime):
+#         # if it is not the end of either linked list
+#         if l1 and l2:
+#             # if value of current node in l1 is larger than the one
+#             # in l2, swap them
+#             if l1.val > l2.val:
+#                 l1, l2 = l2, l1
+            
+#             # set pointer to next node of the smaller value
+#             l1.next = self.mergeTwoLists(l1.next, l2)
+            
+#             # return node of the smaller value
+#             return l1
+        
+#         # return remaining list which has not reached the end
+#         return l1 or l2
